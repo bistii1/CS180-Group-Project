39bfc4dc1d83a97f65e61cfde76f9c8f9c3abc2c
bistii1 <142824913+bistii1@users.noreply.github.com>
2024-11-20 15:27:03 -0500

    Initial commit

---
+++ src/DatabaseInterface.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+/**
+ * Team Project -- Database Interface
+ *
+ * Database interface with all Database methods
+ *
+ * @author Bisti Potdar, lab sec 31
+ *
+ * @version November 3, 2024
+ *
+ */
+
+public interface DatabaseInterface {
+
+    User findUser(String username);
+    void viewUsers();
+    void saveDatabase(String filename) throws IOException;
+    boolean addUser(User user);
+
+    static DatabaseInterface loadDatabase(String filename) throws IOException {
+        return null;
+    }
+
+}
